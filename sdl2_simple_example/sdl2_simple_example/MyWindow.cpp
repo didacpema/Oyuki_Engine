ca2077294df04795d076d994b79c61c3f1c3aeaf
Pep Cots <pec@milestone.dk>
2024-10-02 16:01:11 +0200

    update on window and event processing

--- sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
+++ sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
@@ -1,16 +1,26 @@
+#include <exception>
 #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_opengl.h>
 #include "MyWindow.h"
 using namespace std;
 
-MyWindow::MyWindow(const std::string& title, int w, int h) : _width(w), _height(h) {
+MyWindow::MyWindow(const char* title, unsigned short width, unsigned short height){
+	open(title, width, height);
+}
 
+MyWindow::~MyWindow() {
+    close();
+}
+
+void MyWindow::open(const char* title, unsigned short width, unsigned short height) {
+    if (isOpen()) return;
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    _window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_OPENGL);
+    _window = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
     if (!_window) throw exception(SDL_GetError());
 
     _ctx = SDL_GL_CreateContext(_window);
@@ -19,12 +29,28 @@ MyWindow::MyWindow(const std::string& title, int w, int h) : _width(w), _height(
     if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
 }
 
-MyWindow::~MyWindow() {
+void MyWindow::close() {
+    if (!isOpen()) return;
+
     SDL_GL_DeleteContext(_ctx);
+    _ctx = nullptr;
+
     SDL_DestroyWindow(static_cast<SDL_Window*>(_window));
+	_window = nullptr;
 }
 
 void MyWindow::swapBuffers() const {
     SDL_GL_SwapWindow(static_cast<SDL_Window*>(_window));
+}
+
+bool MyWindow::processEvents(IEventProcessor* event_processor) {
+	SDL_Event e;
+	while (SDL_PollEvent(&e)) {
+		if (event_processor) event_processor->processEvent(e);
+		switch (e.type) {
+		case SDL_QUIT: close(); return false;
+		}
+    }
+    return true;
 }
 
--- sdl2_simple_example/sdl2_simple_example/MyWindow.h
+++ sdl2_simple_example/sdl2_simple_example/MyWindow.h
@@ -1,24 +1,41 @@
 #pragma once
-#include <string>
 
-struct SDL_Window;
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
+
+
+class IEventProcessor {
+public:
+	virtual void processEvent(const SDL_Event& event) = 0;
+};
 
 class MyWindow {
 
 	SDL_Window* _window = nullptr;
 	void* _ctx = nullptr;
 
-	int _width = 0;
-	int _height = 0;
+	unsigned short _width = 800;
+	unsigned short _height = 600;
 
 public:
 	int width() const { return _width; }
 	int height() const { return _height; }
-	double aspectRatio() const { return static_cast<double>(_width) / _height; }
+	double aspect() const { return static_cast<double>(_width) / _height; }
 
-	MyWindow(const std::string& title, int w, int h);
+	auto* windowPtr() const { return _window; }
+	auto* contextPtr() const { return _ctx; }
+
+	MyWindow(const char* title, unsigned short width, unsigned short height);
+	MyWindow(MyWindow&&) noexcept = delete;
+	MyWindow(const MyWindow&) = delete;
+	MyWindow& operator=(const MyWindow&) = delete;
 	~MyWindow();
 
+	void open(const char* title, unsigned short width, unsigned short height);
+	void close();
+	bool isOpen() const { return _window; }
+
+	bool processEvents(IEventProcessor* event_processor = nullptr);
 	void swapBuffers() const;
 
 };
