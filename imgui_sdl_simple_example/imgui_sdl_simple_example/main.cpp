4b70afe32cbdc53c28c4e19dc2c4f314cd20387c
Pep Cots <pec@milestone.dk>
2024-10-02 16:01:51 +0200

    First issue

---
+++ imgui_sdl_simple_example/imgui_sdl_simple_example/main.cpp
@@ -0,0 +1,57 @@
+#include <GL/glew.h>
+#include <chrono>
+#include <thread>
+#include <exception>
+#include <glm/glm.hpp>
+#include "MyWindow.h"
+#include "MyGUI.h"
+using namespace std;
+
+using hrclock = chrono::high_resolution_clock;
+using u8vec4 = glm::u8vec4;
+using ivec2 = glm::ivec2;
+using vec3 = glm::dvec3;
+
+static const ivec2 WINDOW_SIZE(600, 600);
+static const auto FPS = 60;
+static const auto FRAME_DT = 1.0s / FPS;
+
+static void init_openGL() {
+	glewInit();
+	if (!GLEW_VERSION_3_0) throw exception("OpenGL 3.0 API is not available.");
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+}
+
+static void draw_triangle(const u8vec4& color, const vec3& center, double size) {
+	glColor4ub(color.r, color.g, color.b, color.a);
+	glBegin(GL_TRIANGLES);
+	glVertex3d(center.x, center.y + size, center.z);
+	glVertex3d(center.x - size, center.y - size, center.z);
+	glVertex3d(center.x + size, center.y - size, center.z);
+	glEnd();
+}
+
+static void display_func() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	draw_triangle(u8vec4(255, 0, 0, 255), vec3(0.0, 0.0, 0.0), 0.5);
+}
+
+int main(int argc, char** argv) {
+	MyWindow window("ImGUI with SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
+	MyGUI gui(window.windowPtr(), window.contextPtr());
+
+	init_openGL();
+
+	while (window.processEvents(&gui) && window.isOpen()) {
+		const auto t0 = hrclock::now();
+		display_func();
+		gui.render();
+		window.swapBuffers();
+		const auto t1 = hrclock::now();
+		const auto dt = t1 - t0;
+		if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
+	}
+
+	return 0;
+}
