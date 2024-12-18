3d47cea97a66e8806ec93b30c8e996d45eaa8231
miguelturmo <125253347+miguelturmo@users.noreply.github.com>
2024-12-18 10:58:06 +0100

    Revert "priemra parte"
    This reverts commit 5d5758d3dfadbf4581b1ff39c81d2890fdb535e9.

--- sdl2_simple_example/sdl2_simple_example/Jerarquia.h
+++ sdl2_simple_example/sdl2_simple_example/Jerarquia.h
@@ -4,7 +4,6 @@
 
 #include <imgui.h>
 #include "MyWindow.h"
-#include "GameObject.h"
 
 class Jerarquia {
 public:
@@ -13,9 +12,6 @@ public:
 	static void draw();
 
 private:
-	// Método auxiliar para dibujar un objeto y manejar la jerarquía
-	static void drawGameObject(GameObject* gameObject, size_t index);
-
 };
 
 #endif // JERARQUIA_H
